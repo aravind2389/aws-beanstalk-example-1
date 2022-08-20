8a93ab20b44aaddf159185864375ce6fdf591233
aravindk <aravindkundurthi98@gmail.com>
2022-08-20 15:29:11 +0530

    imported in hellocontroller.java

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -2,6 +2,7 @@ package com.techprimers.aws;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequestMapping("/hello")
 @RestController
