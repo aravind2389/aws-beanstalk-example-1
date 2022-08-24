c312b266f3ea0da1a0373123f160494dbb844a20
aravindk <aravindkundurthi98@gmail.com>
2022-08-24 12:10:05 +0530

    multiple request mappings updated

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RequestMapping("/hello")
+@RequestMapping( {"/","/home"} )
 @RestController
 public class HelloController {
     
     @GetMapping
     public String hello() {
-        return "Hello All, Work Together";
+        return "Hello All, Good Morning ";
     }
 }
